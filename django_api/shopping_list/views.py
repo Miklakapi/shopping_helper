5c8d145e9edbf8475e9372df138628636b6fbb4b
Miklakapi <kkmiklaszewski@gmail.com>
2022-05-15 12:56:00 +0000

    urls + views

---
+++ django_api/shopping_list/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+def get(request):
+    pass
+
+def add(request):
+    pass
+
+def delete(request):
+    pass
