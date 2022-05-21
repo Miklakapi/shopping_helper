f1c5841cadf54d1f9a9cf1a8cb1a44fbc24e2328
Miklakapi <kkmiklaszewski@gmail.com>
2022-05-21 17:26:36 +0000

    Auto user api

--- django_api/shopping_list/models.py
+++ django_api/shopping_list/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class ShoppingList(models.Model):
     name = models.CharField(max_length=50)
     quantity = models.IntegerField()
-    owner = models.CharField(max_length=50)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
     date = models.DateField(auto_now_add=True)
 
     class Meta:
--- django_api/shopping_list/serializers.py
+++ django_api/shopping_list/serializers.py
@@ -3,12 +3,20 @@ from .models import ShoppingList
 
 
 class ShoppingListSerializer(serializers.ModelSerializer):
+
+    owner = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    owner_name = serializers.SerializerMethodField(read_only=True)  
+    
     class Meta:
         model = ShoppingList
         fields = [
             'id',
             'name', 
             'quantity', 
-            'owner', 
+            'owner',
+            'owner_name', 
             'date',
         ]
+
+    def get_owner_name(self, obj):
+        return obj.owner.username
